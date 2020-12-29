264e9995f58f8ce32c42a33efc0905f4d15f64d3
vaidas33 <73877931+vaidas33@users.noreply.github.com>
2020-12-29 14:37:15 +0200

    kintamojo pataisymas menesiu pavadinimai

--- js/data.js
+++ js/data.js
@@ -1,14 +1,16 @@
 const account = [
-    { month: 10, income: 150, spalis },
-    { month: 1, income: 200, expense: 50, sausis },
-    { month: 3, income: 450, expense: 200, kovas },
-    { month: 7, expense: 50, liepa },
-    { month: 2, income: 50, vasaris },
-    { month: 5, income: 150, expense: 50, geguze },
-    { month: 9, income: 700, expense: 400, rugsejis },
-    { month: 4, income: 10, expense: 300, balandis },
-    { month: 6, expense: 50, birzelis },
-    { month: 11, income: 150, expense: 30, lapkritis },
-    { month: 8, income: 450, expense: 120, rugpjutis },
-    { month: 12, expense: 200, gruodis }
+    { month: 10, income: 150, pavadinimas: 'spalis' },
+    { month: 1, income: 200, expense: 50, pavadinimas: 'sausis' },
+    { month: 3, income: 450, expense: 200, pavadinimas: 'kovas' },
+    { month: 7, expense: 50, pavadinimas: 'liepa' },
+    { month: 2, income: 50, pavadinimas: 'vasaris' },
+    { month: 5, income: 150, expense: 50, pavadinimas: 'geguze' },
+    { month: 9, income: 700, expense: 400, pavadinimas: 'rugsejis' },
+    { month: 4, income: 10, expense: 300, pavadinimas: 'balandis' },
+    { month: 6, expense: 50, pavadinimas: 'birzelis' },
+    { month: 11, income: 150, expense: 30, pavadinimas: 'lapkritis' },
+    { month: 8, income: 450, expense: 120, pavadinimas: 'rugpjutis' },
+    { month: 12, expense: 200, pavadinimas: 'gruodis' }
 ];
+
+export { account }
