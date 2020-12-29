65bebaa2034479f80e6cbcbe7ff16d6ae599f8cb
vaidas33 <73877931+vaidas33@users.noreply.github.com>
2020-12-29 14:50:32 +0200

    data.js kintamuju pataisymas

--- js/data.js
+++ js/data.js
@@ -1,16 +1,16 @@
 const account = [
-    { month: 10, income: 150, pavadinimas: 'spalis' },
-    { month: 1, income: 200, expense: 50, pavadinimas: 'sausis' },
-    { month: 3, income: 450, expense: 200, pavadinimas: 'kovas' },
-    { month: 7, expense: 50, pavadinimas: 'liepa' },
-    { month: 2, income: 50, pavadinimas: 'vasaris' },
-    { month: 5, income: 150, expense: 50, pavadinimas: 'geguze' },
-    { month: 9, income: 700, expense: 400, pavadinimas: 'rugsejis' },
-    { month: 4, income: 10, expense: 300, pavadinimas: 'balandis' },
-    { month: 6, expense: 50, pavadinimas: 'birzelis' },
-    { month: 11, income: 150, expense: 30, pavadinimas: 'lapkritis' },
-    { month: 8, income: 450, expense: 120, pavadinimas: 'rugpjutis' },
-    { month: 12, expense: 200, pavadinimas: 'gruodis' }
+    { month: 10, income: 150 },
+    { month: 1, income: 200, expense: 50 },
+    { month: 3, income: 450, expense: 200 },
+    { month: 7, expense: 50 },
+    { month: 2, income: 50 },
+    { month: 5, income: 150, expense: 50 },
+    { month: 9, income: 700, expense: 400 },
+    { month: 4, income: 10, expense: 300 },
+    { month: 6, expense: 50 },
+    { month: 11, income: 150, expense: 30 },
+    { month: 8, income: 450, expense: 120 },
+    { month: 12, expense: 200 }
 ];
 
 const menesiuPavadinimai = [
@@ -26,6 +26,6 @@ const menesiuPavadinimai = [
     { pavadinimas: 'lapkritis' },
     { pavadinimas: 'rugpjutis' },
     { pavadinimas: 'gruodis' }
-]
+];
 
 export { account, menesiuPavadinimai }
