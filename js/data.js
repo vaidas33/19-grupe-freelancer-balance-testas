840e4ff0adf176e042ede7bff6a9e91fc89e95d6
vaidas33 <73877931+vaidas33@users.noreply.github.com>
2020-12-29 14:01:41 +0200

    kintamajame uzpildyti menesiu pavadinimai

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
 const account = [
-    { month: 10, income: 150 },
-    { month: 1, income: 200, expense: 50 },
-    { month: 3, income: 450, expense: 200 },
-    { month: 7, expense: 50 },
-    { month: 2, income: 50 },
-    { month: 5, income: 150, expense: 50 },
-    { month: 9, income: 700, expense: 400 },
-    { month: 4, income: 10, expense: 300 },
-    { month: 6, expense: 50 },
-    { month: 11, income: 150, expense: 30 },
-    { month: 8, income: 450, expense: 120 },
-    { month: 12, expense: 200 }
+    { month: 10, income: 150, spalis },
+    { month: 1, income: 200, expense: 50, sausis },
+    { month: 3, income: 450, expense: 200, kovas },
+    { month: 7, expense: 50, liepa },
+    { month: 2, income: 50, vasaris },
+    { month: 5, income: 150, expense: 50, geguze },
+    { month: 9, income: 700, expense: 400, rugsejis },
+    { month: 4, income: 10, expense: 300, balandis },
+    { month: 6, expense: 50, birzelis },
+    { month: 11, income: 150, expense: 30, lapkritis },
+    { month: 8, income: 450, expense: 120, rugpjutis },
+    { month: 12, expense: 200, gruodis }
 ];
